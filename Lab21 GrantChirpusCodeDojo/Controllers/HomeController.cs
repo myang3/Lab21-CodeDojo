9f8bf84e811891a633d6cc3fc3a2aae12d3ec068
moyang9192 <moyang9192@gmail.com>
2016-09-27 16:34:45 -0400

    Finished Form

---
+++ Lab21 GrantChirpusCodeDojo/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab21_GrantChirpusCodeDojo.Models;
+
+namespace Lab21_GrantChirpusCodeDojo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            //condition then else
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(StudentResponse studentResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email response to the party organizer
+                return View("Thanks", studentResponse);
+            }
+            else
+            { //there is a validation error
+                return View();
+            }
+        }
+    }
+}
---
+++ Lab21 GrantChirpusCodeDojo/Models/StudentResponse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab21_GrantChirpusCodeDojo.Models
+{
+    public class StudentResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please select your gender")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        //(ReularExpression is a special pattern . = at least one, + =none or many more)
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify your course selection")]
+        public bool? WillAttend { get; set; }
+        //bool? takes 3 arguement (true,false,null)
+
+        public bool WebsiteSearch { get; set; }
+        public bool Newspaper { get; set; }
+        public bool Radio { get; set; }
+        public bool FriendsFamily { get; set; }
+        public bool SocialMedia { get; set; }
+        public bool Other { get; set; }
+
+
+    }
+}
